a35ae3c369a007631dd613a3d55cb1d2d4493082
manuatiini <mauno.juuma@gmail.com>
2023-06-09 11:21:01 +0300

    valmis

--- hyppypeli/hyppypeli/hyppypeli.cs
+++ hyppypeli/hyppypeli/hyppypeli.cs
@@ -17,7 +17,7 @@ public class hyppypeli : PhysicsGame
 
     private Image pelaajanKuva;
     private Image morkokuva = LoadImage("mörkö.png");
-    private Image tahtiKuva = LoadImage("tahti.png");
+    private Image tahtikuva = LoadImage("tahti.png");
     private Image estekuva = LoadImage("piikki.png");
 
     private SoundEffect maaliAani = LoadSoundEffect("maali.wav");
@@ -44,26 +44,19 @@ public class hyppypeli : PhysicsGame
 
     }
 
-    void lisaamörkö(Vector paikka, double leveys, double korkeus) 
-    {PhysicsObject morko = new PhysicsObject(40, 20);
+    void lisaamörkö(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject morko = new PhysicsObject(40, 20);
         morko.Image = morkokuva;
-    Add(morko);
-    morko.Tag = "morko";
-    morko.Position = paikka; 
-    FollowerBrain seuraajanAivot = new FollowerBrain(pelaaja1);
-    seuraajanAivot.Speed = 150;
-    morko.Brain = seuraajanAivot;
+        Add(morko);
+        morko.Tag = "morko";
+        morko.Position = paikka;
+        FollowerBrain seuraajanAivot = new FollowerBrain(pelaaja1);
+        seuraajanAivot.Speed = 150;
+        morko.Brain = seuraajanAivot;
+        AddCollisionHandler(morko, TormasiEsteeseen);
     }
 
-void törmäävät (PhysicsObject tormaaja, PhysicsObject kohde)
-{
-    tormaaja.Destroy(); 
-    kuolemaaani.Play();
-    ClearAll();
-    LuoKentta(); 
-    LisaaNappaimet();
-    Camera.Follow(pelaaja1);
-}
     private void LuoKentta()
     {
         TileMap kentta = TileMap.FromLevelAsset("kentta1.txt");
@@ -71,7 +64,7 @@ void törmäävät (PhysicsObject tormaaja, PhysicsObject kohde)
         kentta.SetTileMethod('*', LisaaTahti);
         kentta.SetTileMethod('N', LisaaPelaaja);
         kentta.SetTileMethod('e', LisaaEste);
-        kentta.SetTileMethod('M',lisaamörkö );
+        kentta.SetTileMethod('M', lisaamörkö);
         kentta.Execute(RUUDUN_KOKO, RUUDUN_KOKO);
         Level.CreateBorders();
         Level.Background.CreateGradient(Color.White, Color.SkyBlue);
@@ -87,20 +80,22 @@ void törmäävät (PhysicsObject tormaaja, PhysicsObject kohde)
 
         Add(este);
     }
-   
-    
 
 
-private void TormasiEsteeseen(PhysicsObject tormaaja, PhysicsObject kohde)
+
+
+    private void TormasiEsteeseen(PhysicsObject tormaaja, PhysicsObject kohde)
     {
+        if (kohde != pelaaja1) return;
         kohde.Destroy();
         kuolemaaani.Play();
         ClearAll();
-        LuoKentta(); 
-        LisaaNappaimet();
-        Camera.Follow(pelaaja1);
+        Timer.SingleShot(2, Begin);
     }
+
+
     SoundEffect kuolemaaani = LoadSoundEffect("super-mario-death-sound-sound-effect.wav");
+
     private void LisaaTaso(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
@@ -109,15 +104,7 @@ private void TormasiEsteeseen(PhysicsObject tormaaja, PhysicsObject kohde)
         Add(taso);
     }
 
-    private void LisaaTahti(Vector paikka, double leveys, double korkeus)
-    {
-        PhysicsObject tahti = PhysicsObject.CreateStaticObject(leveys, korkeus);
-        tahti.IgnoresCollisionResponse = true;
-        tahti.Position = paikka;
-        tahti.Image = tahtiKuva;
-        tahti.Tag = "tahti";
-        Add(tahti);
-    }
+
 
     private void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
     {
@@ -127,12 +114,12 @@ private void TormasiEsteeseen(PhysicsObject tormaaja, PhysicsObject kohde)
             Mass = 4.0,
             Image = pelaajanKuva
         };
-        AddCollisionHandler(pelaaja1, "tahti", TormaaTahteen);
         Add(pelaaja1);
-        AddCollisionHandler(pelaaja1, "morko" ,törmäävät);
+        AddCollisionHandler(pelaaja1, Tormaatahteen);
+
     }
 
-    private void LisaaNappaimet()
+    void LisaaNappaimet()
     {
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
@@ -145,26 +132,43 @@ private void TormasiEsteeseen(PhysicsObject tormaaja, PhysicsObject kohde)
 
         ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1,
             -NOPEUS);
-        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, NOPEUS);
+        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1,
+            NOPEUS);
         ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
     }
 
-    private void Liikuta(PlatformCharacter hahmo, double nopeus)
+    void Liikuta(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Walk(nopeus);
     }
 
-    private void Hyppaa(PlatformCharacter hahmo, double nopeus)
+    void Hyppaa(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Jump(nopeus);
     }
 
-    private void TormaaTahteen(PhysicsObject hahmo, PhysicsObject tahti)
+    private void Tormaatahteen(PhysicsObject pelaaja1, PhysicsObject kohde)
     {
-        maaliAani.Play();
-        MessageDisplay.Add("Keräsit tähden!");
-        tahti.Destroy();
+        if (kohde.Tag == "tahti")
+        {
+            maaliAani.Play();
+            MessageDisplay.Add("wow tähti!");
+            kohde.Destroy();
+        }
+        
+            
+        
+    }
+
+    private void LisaaTahti(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject tahti = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        tahti.IgnoresCollisionResponse = true;
+        tahti.Position = paikka;
+        tahti.Image = tahtikuva;
+        tahti.Tag = "tahti";
+        Add(tahti);
     }
 }
